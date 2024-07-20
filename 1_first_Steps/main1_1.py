54b03048fb030aac0b4ab32ac751252005a443dc
PDThang <phuongdaithang2004@gmail.com>
2024-07-20 14:19:20 +0700

    Initial commit

--- 1_first_Steps/main1_1.py
+++ 1_first_Steps/main1_1.py
@@ -5,4 +5,4 @@ app = FastAPI() #create a FastAPI "instance"
 @app.get("/") #create a path operation
 def read_root(): #define the path operation function
     return {"Hello": "Thang"} # return the content
-# uvicorn main1_1: app --reload
+# uvicorn main1_1:app --reload
